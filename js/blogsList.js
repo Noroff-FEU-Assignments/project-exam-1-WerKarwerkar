e12a7ddcf7e6bb6cf6d3f096e9067b2bfda84f35
WerKarwerkar <126430261+WerKarwerkar@users.noreply.github.com>
2023-11-17 15:25:07 +0100

    date

--- js/blogsList.js
+++ js/blogsList.js
@@ -23,13 +23,13 @@ async function fetchPosts() {
     for (let i = 0; i < posts.length; i++) {
       const post = posts[i];
 
-      const postDate = new Date(post.date);
+      const formattedDate = post.date.toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' });
 
 
 
       detailContainer.innerHTML += `<a href="specific-post.html?id=${post.id}" class="card">
                                 <h2>${post.title.rendered}</h2>
-                                <h3></h3>
+                                <h3>${formattedDate}</h3>
                                 <h4>${post.better_featured_image.alt_text}</h4>
                                 <img src="${post.better_featured_image.source_url}">
                                 </a>`;
