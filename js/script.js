41c65fcdb1909be418ec23dccfdfacc715c0e79e
WerKarwerkar <126430261+WerKarwerkar@users.noreply.github.com>
2023-11-23 15:23:32 +0100

    no

--- js/script.js
+++
@@ -1,7 +0,0 @@
-const hamburgerBtn = document.querySelector('button.hamburger');
-const nav = document.querySelector('nav.navigation');
-
-hamburgerBtn.addEventListener('click', () => {
-  nav.classList.toggle('is-open');
-  document.body.classList.toggle('no-scroll');
-});
