0bebe392916f22bec9b8e8a60111333f24d45ee0
WerKarwerkar <126430261+WerKarwerkar@users.noreply.github.com>
2023-11-27 21:23:14 +0100

    ja

--- js/home.js
+++ js/home.js
@@ -2,7 +2,7 @@ const initialSlider = document.querySelector('.slider');
 const leftArrow = document.querySelector('.left');
 const rightArrow = document.querySelector('.right');
 
-var sectionIndex = 0;
+let sectionIndex = 0;
 
 leftArrow.addEventListener('click', function() {
     sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 0;
@@ -14,4 +14,32 @@ rightArrow.addEventListener('click', function() {
     initialSlider.style.transform = 'translate(' + (sectionIndex * -25) + '%)';
 });
 
+async function fetchData() {
+    const url = "https://www.weronkakarczmarczyk.no/wp-json/wp/v2/posts";
 
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching the posts:', error);
+    }
+}
+
+
+async function updateCarousel() {
+    const postData = await fetchData();
+    const postSections = postData.map(post => {
+        return `<section>
+                   <div>
+                     <a href="html/single-blog.html?id=${post.id}&title=${post.title.rendered}}"><img src="${post.better_featured_image.source_url}" alt="${post.better_featured_image.source_url}" class="latest-posts"/></a>
+                   </div>
+                 </section>`;
+    });
+
+    initialSlider.innerHTML = postSections.join('');
+
+    sectionIndex = 0;
+}
+
+updateCarousel();
