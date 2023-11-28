8a65c27571f6338297fc01693d64ce73ce72b675
WerKarwerkar <126430261+WerKarwerkar@users.noreply.github.com>
2023-11-28 17:50:57 +0100

    modal

--- js/specificPost.js
+++ js/specificPost.js
@@ -3,7 +3,8 @@ const detailPosts = document.getElementById("post-details");
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("id");
-
+const modal = document.getElementById("myModal");
+const modalContent = document.getElementById("modal-content");
 
 if (!id) {
   showError("Post ID is not found in the query string");
@@ -13,19 +14,19 @@ if (!id) {
 
   async function fetchPost() {
     try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error('Failed to fetch post with that id');
-        }
-        const details = await response.json();
-        createHtml(details);
-        document.title = details.title.rendered;
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error('Failed to fetch post with that id');
+      }
+      const details = await response.json();
+      createHtml(details);
+      document.title = details.title.rendered;
     } catch (error) {
-        showError(error.message);
+      showError(error.message);
     } finally {
-        loadingIndicator.style.display = "none";
+      loadingIndicator.style.display = "none";
     }
-}
+  }
 
   fetchPost();
 
@@ -42,5 +43,21 @@ if (!id) {
                                 <h3>${details.content.rendered}</h3>
                               </div>`;
     document.title = details.title.rendered;
-}
+
+    const createModal = detailPosts.querySelectorAll(".wp-block-image");
+
+    createModal.forEach((imageWp) => {
+      const getWpImg = imageWp.innerHTML;
+      imageWp.addEventListener("click", () => {
+        modal.showModal();
+        modalContent.innerHTML = `<div class="contentPost">
+                                    ${getWpImg}
+                                  </div>`;
+      });
+    });
+
+    modal.addEventListener("click", () => {
+      modal.close();
+    });
+  }
 }
